02cb2a821195fc9e4fd4445abab872415433eee4
abhishekmenon970 <abhishekvmenon970@gmail.com>
2018-03-28 15:13:40 +0530

    commit

---
+++ amst.sh
@@ -0,0 +1,18 @@
+echo "enter limit"
+read a 
+read b
+for((i=$a;i<=$b;i++))
+do
+s=0
+n=$i
+while [ $n -gt 0 ]
+do
+r=` expr $n % 10 `
+s=` expr $s + $r \* $r \* $r `
+n=` expr $n / 10 `
+done
+if [ $s -eq $i ];then
+echo "$i"
+fi
+done
+
